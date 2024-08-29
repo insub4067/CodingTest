d3a8b4604c17bc9f3d83f3499e2daead3c366f9c
insub <vb15@kakao.com>
2024-08-29 17:03:34 +0900

    Update 곱하기 또는 더하기.py

--- 곱하기 또는 더하기.py
+++ 곱하기 또는 더하기.py
@@ -1,15 +1,12 @@
 // 곱하기 또는 더하기
 
-input = list(map(int, list("567"))) 
+input = list(map(int, list("567")))
+answer = input[0]
 
-for idx, element in enumerate(input) :
-    current = input[idx]
-    hasNext = len(input) >= idx + 2 
-    if hasNext:
-        next = input[idx + 1]
-        if current == 0 or next == 0:
-            input[idx + 1] = current + next 
-        else:
-            input[idx + 1] = current * next 
+for num in input[1:]:
+    if num <= 1 or answer <= 1:
+        answer += num 
     else:
-        print(input[idx])
+        answer *= num 
+
+print(answer)
