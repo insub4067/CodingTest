a50be8a97e0c15e52fe34ab217983254e2eec474
insub <vb15@kakao.com>
2024-09-01 19:12:50 +0900

    Update 미로 탈출.py

--- 동빈나/DFS BFS/미로 탈출.py
+++ 동빈나/DFS BFS/미로 탈출.py
@@ -2,19 +2,30 @@
 
 # 나의 풀이
 n, m = 5, 6
-graph = [[1,0,1,0,1,0], [1,1,1,1,1,1], [0,0,0,0,0,1], [1,1,1,1,1,1], [1,1,1,1,1,1]]
-
-result = 1
-execute = True
+graph = [
+    [1,0,1,0,1,0], 
+    [1,1,1,1,1,1], 
+    [0,0,0,0,0,1], 
+    [1,1,1,1,1,1], 
+    [1,1,1,1,1,1]
+]
+visited = [[False] * m for _ in range(n)]
 
 def dfs(x, y, count):
+    visited[y][x] = True
+    # 도착시 종료
     if  x == m - 1 and y == n - 1:
         print(count)
-    if 0 <= x < m - 1:
-        if graph[y][x + 1] == 1:
-            dfs(x + 1, y, count + 1)
-    if 0 <= y < n - 1:
-        if graph[y + 1][x] == 1:
-            dfs(x, y + 1, count + 1)
+        return 
+    
+
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+    # 다음 노드 탐색
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        # 범위안이고, 방문하지 않았고, 장애물이 없다면 다음으로 진행
+        if 0 <= nx < m and 0 <= ny < n and not visited[ny][nx] and graph[ny][nx] == 1:
+            dfs(nx, ny, count + 1)
             
 dfs(0, 0, 1)
