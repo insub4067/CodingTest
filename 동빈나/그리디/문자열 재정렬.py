0f555aa1e13a49f9be99c63ccb84d7d072d3d523
insub <vb15@kakao.com>
2024-09-01 14:07:47 +0900

    Create 문자열 재정렬.py

---
+++ 동빈나/그리디/문자열 재정렬.py
@@ -0,0 +1,18 @@
+// 문자열 재정렬.py
+
+input = "K1KA5CB7"
+input = "AJKDLSI412K4JSJ9D"
+
+l = list(input)
+l.sort()
+
+s = ""
+sum = 0
+
+for i in l:
+    if i.isdigit():
+        sum += int(i)
+    else:
+        s += i
+
+print(s + str(sum))
