da921fd25e4e51c49f91cc20c5e30a9c3b198fdb
insub <vb15@kakao.com>
2024-09-02 14:55:48 +0900

    Create 타겟 넘버.py

---
+++ 프로그래머스/DFS BFS/타겟 넘버.py
@@ -0,0 +1,20 @@
+# 타겟 넘버.py
+
+def solution(numbers, target):
+    
+    n = len(numbers)
+    answer = 0
+    
+    def dfs(i, num):
+        nonlocal answer
+        if i == n:
+            if num == target:
+                answer += 1
+            return
+        
+
+        dfs(i + 1, num + numbers[i])
+        dfs(i + 1, num - numbers[i])
+    
+    dfs(0, 0)
+    return answer
