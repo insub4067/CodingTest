dce08507d8adeed3276ec7714d032c929281c337
insub <vb15@kakao.com>
2024-09-26 14:24:56 +0900

    Create 4949번 균형잡힌 세상.py

---
+++ 백준/4949번 균형잡힌 세상.py
@@ -0,0 +1,31 @@
+# 4949번 균형잡힌 세상.py
+# https://www.acmicpc.net/problem/4949
+
+while True:
+    line = input()  
+    if line == ".": 
+        break
+    
+    stack = []
+    balanced = True
+    
+    for char in line:
+        if char == "(" or char == "[":
+            stack.append(char)
+        elif char == ")":
+            if stack and stack[-1] == "(":
+                stack.pop()
+            else:
+                balanced = False
+                break
+        elif char == "]":
+            if stack and stack[-1] == "[":
+                stack.pop()
+            else:
+                balanced = False
+                break
+    
+    if balanced and not stack:
+        print("yes")
+    else:
+        print("no")
