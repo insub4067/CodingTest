3d3158329375292f89632f8330b525bf92ac17ba
insub <vb15@kakao.com>
2024-09-28 02:18:14 +0900

    Update 연속된 부분 수열의 합.py

--- 프로그래머스/연속된 부분 수열의 합.py
+++ 프로그래머스/연속된 부분 수열의 합.py
@@ -1,4 +1,6 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/178870?language=python3
+
+# 1번째 시도 (시간 초과)
 # 채점 결과
 # 정확성: 47.1
 # 합계: 47.1 / 100.0
@@ -15,4 +17,27 @@ def solution(sequence, k):
             if temp_sum == k: 
                 if not answer or (answer[1] - answer[0]) > (ii - i):
                     answer = [i, ii]
+    return answer
+
+
+
+# ------------------------------------
+# 2번째 시도(통과)
+
+def solution(sequence, k):
+    start = 0
+    current_sum = 0
+    answer = []
+    
+    for end in range(len(sequence)):
+        current_sum += sequence[end] 
+        
+        while current_sum > k and start <= end:
+            current_sum -= sequence[start]
+            start += 1
+        
+        if current_sum == k:
+            if not answer or (end - start) < (answer[1] - answer[0]):
+                answer = [start, end]
+    
     return answer
