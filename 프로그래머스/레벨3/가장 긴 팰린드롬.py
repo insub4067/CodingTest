a9aca01c548e20611ae283903e70bd22901d82bb
insub <vb15@kakao.com>
2024-10-02 12:48:41 +0900

    Update 가장 긴 팰린드롬.py

--- 프로그래머스/레벨3/가장 긴 팰린드롬.py
+++ 프로그래머스/레벨3/가장 긴 팰린드롬.py
@@ -32,20 +32,22 @@ def solution(s):
 
 # ---------------------------
 # 2차 시도
+# 채점 결과
+# 정확성: 72.1
+# 효율성: 27.9
+# 합계: 100.0 / 100.0
 
 def solution(s):
 
     answer = 0
+
     for i in range(len(s)):
         for j in range(i + 1, len(s) + 1):
             substring = s[i:j]
-            isEqual = substring == substring[::-1]
-            if isEqual:
+            if substring == substring[::-1]:
                 answer = max(answer, j - i)
     
     return answer
-
-
 
 # expect: 7
 print(solution("abcdcba"))
