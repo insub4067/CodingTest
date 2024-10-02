92be88adeed04e7854935787d8aa85216af7a164
insub <vb15@kakao.com>
2024-10-02 12:35:41 +0900

    Create 가장 긴 팰린드롬.py

---
+++ 프로그래머스/레벨3/가장 긴 팰린드롬.py
@@ -0,0 +1,39 @@
+# 프로그래머스: 레벨3 가장 긴 팰린드롬
+# https://school.programmers.co.kr/learn/courses/30/lessons/12904
+
+# ---------------------------
+# 1차 시도
+# 채점 결과
+# 정확성: 51.1
+# 효율성: 0.0
+# 합계: 51.1 / 100.0
+
+def solution(s):
+    answer = 0
+    length = len(s)
+
+    def compare(i, offset):
+        next = offset + 1
+        if (i - next) in range(len(s)) and (i + next) in range(len(s)):
+            pre = s[i - next]
+            follow = s[i + next]
+            if pre == follow:
+                return compare(i, next)
+            else:
+                return offset
+        else:
+            return offset
+    
+    for i in range(len(s)):
+        result = compare(i, 0) * 2 
+        answer = max(answer, result + 1)
+
+    return answer
+
+# ---------------------------
+# 2차 시도
+
+
+
+# expect: 7
+print(solution("abcdcba"))
