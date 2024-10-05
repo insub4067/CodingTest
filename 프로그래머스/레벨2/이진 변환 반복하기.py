30b9c9c1e1c0086f7bea1429b8a42bbfa898f7a1
insub <vb15@kakao.com>
2024-10-06 02:00:51 +0900

    Create 이진 변환 반복하기.py

---
+++ 프로그래머스/레벨2/이진 변환 반복하기.py
@@ -0,0 +1,23 @@
+# 프로그래머스 레벨2: 이진 변환 반복하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/70129
+
+def solution(s):
+    answer = []
+    stage = 0
+    zeros = 0
+
+    while True:
+        stage += 1
+        zeros += s.count("0")
+        s = s.replace("0", "")
+        if s == "1":
+            break
+        s = bin(len(s))[2:]
+
+    answer.append(stage)
+    answer.append(zeros)
+    return answer
+
+input = "110010101001"
+# except: [3,8]
+print(solution(input))
