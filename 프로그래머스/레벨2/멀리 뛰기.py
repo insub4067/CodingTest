680eaaeceae4f0afbd4e7c3999c5be303b4a85d4
insub <vb15@kakao.com>
2024-10-06 20:55:41 +0900

    Update 멀리 뛰기.py

--- 프로그래머스/레벨2/멀리 뛰기.py
+++ 프로그래머스/레벨2/멀리 뛰기.py
@@ -31,7 +31,6 @@ def solution(n):
     dp[1] = 1  
     
     for i in range(2, n + 1):
-        print(i, dp[i - 1], dp[i - 2])
         dp[i] = dp[i - 1] + dp[i - 2]
     
     return dp[n] % 1234567
