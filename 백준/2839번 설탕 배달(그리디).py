b180a561df4a3ff52d92692f194109e95ac81cc1
insub <vb15@kakao.com>
2024-10-07 16:43:45 +0900

    Create 2839번 설탕 배달(그리디).py

---
+++ 백준/2839번 설탕 배달(그리디).py
@@ -0,0 +1,22 @@
+# 설탕 배달
+# https://www.acmicpc.net/problem/2839
+
+N = 18
+answer = 0
+
+while True:
+
+    if N % 5 == 0:
+        answer += N // 5
+        N %= 5
+        print(answer)
+        break
+
+    N -= 3
+    answer += 1
+
+    if N < 0:
+        print(-1)
+        break
+
+    
