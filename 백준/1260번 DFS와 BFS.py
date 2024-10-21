4dab92d972d8b79ef24b3b8e31be7e1faa6b99bf
insub <vb15@kakao.com>
2024-10-22 06:39:34 +0900

    Create 1260번 DFS와 BFS.py

---
+++ 백준/1260번 DFS와 BFS.py
@@ -0,0 +1,19 @@
+from collections import deque
+from collections import defaultdict
+
+V, N, V = 4, 5, 1
+
+
+dict = defaultdict(int)
+
+
+# def bfs():
+#     visited = []
+#     q = deque()
+#     q.append(routes[0])
+
+# def dfs():
+
+
+# bfs()
+# dfs()
