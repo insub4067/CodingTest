42d7e51e5846aecde58a608e8813925548c51d11
insub <vb15@kakao.com>
2024-10-22 06:52:41 +0900

    Update 정수 제곱근 판별.py

--- 프로그래머스/레벨1/정수 제곱근 판별.py
+++ 프로그래머스/레벨1/정수 제곱근 판별.py
@@ -1,11 +1,10 @@
 # 정수 제곱근 판별
 
 def solution(n):
-    
+
     cnt = 0
-    limit = 50000000000000
     
-    while cnt < limit:
+    while cnt < 50000000000000:
         cnt += 1
         squared = cnt ** 2
         if squared == n:
