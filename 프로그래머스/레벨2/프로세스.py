14b54d669aa5f4660046f37d2f75d13bbbb3ab64
insub <vb15@kakao.com>
2024-11-07 10:57:30 +0900

    Update 프로세스.py

--- 프로그래머스/레벨2/프로세스.py
+++ 프로그래머스/레벨2/프로세스.py
@@ -1,31 +1,68 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/42587
+def findShortestPath(startIndex, target, alphas):
+    i = alphas.index(target)
+    total_l = len(alphas)
+    forward = abs(i - startIndex)
+    backward = total_l - forward 
+    return min(forward, backward)
 
-from collections import deque
-
-def solution(priorities, location):
+def solution(name):
+    alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     answer = 0
-    
-    # (인덱스넘버, 우선순위)
-    t_priorities = []
-    
-    # 인덱스, 우선순위
-    for i, p in enumerate(priorities):
-        # (우선순위, 인덱스넘버)
-        t_p = (i, p)
-        t_priorities.append(t_p)
-        
-    priorities.sort()
-    
-    dq = deque(t_priorities)
-    while dq:
-        i, p = dq.popleft()
-        top_p = priorities[len(priorities) - 1]
-        if top_p > p:
-            dq.append((i, p))
-            continue
+    cursor = 0
+    next = 0
+
+    for e in name:
+        if e != "A":
+            alphaMove = findShortestPath(0, e, alphabets)  
+            answer += alphaMove
+
+    for i, e in enumerate(name):
+        if e != "A":
+            next = i
+            break
+
+    answer += findShortestPath(cursor, next, range(len(name)))
+    cursor = next
+
+    for c in name[next + 1:]:
+        if c != "A":
+            answer += 1  
+            cursor += 1
         else:
-            priorities.pop()
-            answer += 1
-        
-        if i == location:
-            return answer
+            answer += findShortestPath(cursor, next, range(len(name)))
+            cursor = next
+            
+    return answer
+
+
+# def solution(name):
+#     name = list(map(str, name))
+#     alphabets = [
+#         "A", "B", "C", "D", "E", 
+#         "F", "G", "H", "I", "J", 
+#         "K", "L", "M", "N", "O", 
+#         "P", "Q", "R", "S", "T", 
+#         "U", "V", "W", "X", "Y", "Z"
+#     ]
+#     total_l = len(alphabets)
+    
+#     answer = 0
+#     current = ["A"] * len(name)
+#     cursor = 0
+
+#     for i, e in enumerate(name):
+#         if e == "A":
+#             continue
+
+#         cursorMove = findShortestPath(cursor, e, name)
+#         answer += cursorMove
+
+#         alphaMove = findShortestPath(0, e, alphabets)
+#         answer += alphaMove
+
+#         cursor = i
+            
+#     return answer
+
+result = solution("JAZ")
+print(result)
